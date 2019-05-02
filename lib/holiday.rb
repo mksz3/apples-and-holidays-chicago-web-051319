2fd9f0509a7e7d69203d450ebe64fcb334d27655
Markus Sanders <markus.sanderst@gmail.com>
2019-05-02 23:46:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.
+  holiday_hash.select {
+    |holidays, supply|
+    holidays.include?('BBQ')
+  }
 end
 
 
